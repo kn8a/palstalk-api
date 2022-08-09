d7da180054542e097256be75d587ee4704e1b637
kn8a <88045655+kn8a@users.noreply.github.com>
2022-08-09 15:13:13 +0700

    prep comments and posts controllers

---
+++ controllers/commentController.js
@@ -0,0 +1,17 @@
+const Comment = require('../models/commentModel')
+const Post = require('../models/postModel')
+const User = require('../models/userModel')
+const asyncHandler = require('express-async-handler')
+
+//^ get all comments for post
+
+//^ create a single comment to blog-post
+
+//^ comment edit - add "isEdited" field to model
+
+//^ comment like
+
+//^ delete a comment 
+
+//^ report comment
+
---
+++ controllers/postController.js
@@ -0,0 +1,13 @@
+const User = require('../models/userModel')
+const Post = require('../models/postModel')
+const asyncHandler = require('express-async-handler')
+
+//! Create post
+
+//! Edit post
+
+//! delete post
+
+//! like post
+
+//! report post
