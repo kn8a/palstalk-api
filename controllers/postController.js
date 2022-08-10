da70ee0207d1c64d8dd561e22343350c897aa932
kn8a <88045655+kn8a@users.noreply.github.com>
2022-08-10 11:13:54 +0700

    add additional fields to post and comment models

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,11 +3,26 @@ const Post = require('../models/postModel')
 const asyncHandler = require('express-async-handler')
 
 //! Create post
+const createPost = asyncHandler( async (req,res) => {
+
+})
 
 //! Edit post
+const editPost = asyncHandler( async (req,res) => {
+    
+})
 
 //! delete post
+const deletePost = asyncHandler( async (req,res) => {
+    
+})
 
 //! like post
+const likePost = asyncHandler( async (req,res) => {
+    
+})
 
 //! report post
+const reportPost = asyncHandler( async (req,res) => {
+    
+})
