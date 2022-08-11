df476fdf14c7c8bef45d9064481b3aebf02b1f28
kn8a <88045655+kn8a@users.noreply.github.com>
2022-08-11 11:34:19 +0700

    add comment routes & initiate comments controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,15 +3,42 @@ const Post = require('../models/postModel')
 const User = require('../models/userModel')
 const asyncHandler = require('express-async-handler')
 
-//^ get all comments for post
+//^ get all comments for post - viewer/admin
+const getCommentsForPost = asyncHandler( async (req,res) => {
+    
+})
 
 //^ create a single comment to blog-post
+const createComment = asyncHandler( async (req,res) => {
+    
+})
 
-//^ comment edit - add "isEdited" field to model
-
-//^ comment like
+//^ comment edit - add "isEdited" field to model - only author
+const editComment = asyncHandler( async (req,res) => {
+    
+})
 
 //^ delete a comment 
+const deleteComment = asyncHandler( async (req,res) => {
+    
+})
+
+//^ comment like
+const likeComment = asyncHandler( async (req,res) => {
+    
+})
+
+//^ comment unlike
+const unlikeComment = asyncHandler( async (req,res) => {
+    
+})
 
 //^ report comment
+const reportComment = asyncHandler( async (req,res) => {
+    
+})
 
+
+module.exports = {
+    getCommentsForPost, createComment, editComment, likeComment, unlikeComment, reportComment, deleteComment
+}
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const { route } = require('.');
 const { protect } = require('../middleware/authMiddleware');
 var router = express.Router();
 const {createPost, getPost, editPost, deletePost, likePost, reportPost, unlikePost} = require('../controllers/postController')
+const {getCommentsForPost, createComment, editComment, likeComment, unlikeComment, reportComment, deleteComment} = require('../controllers/commentController')
 
 router.post('/', protect, createPost)
 router.put('/:postId', protect, editPost)
@@ -11,6 +12,13 @@ router.put('/:postId/unlike', protect, unlikePost)
 router.put('/:postId/report', protect, reportPost)
 
 
+router.get('/:postId/comments', protect, getCommentsForPost)
+router.post('/:postId/comments', protect, createComment) 
+router.put('/:postId/:commentId', protect, editComment) 
+router.put('/:postId/:commentId/like', protect, likeComment) 
+router.put('/:postId/:commentId/unlike', protect, unlikeComment) 
+router.put('/:postId/:commentId/report', protect, reportComment)
 
+router.delete('/:postId/:commentId/delete', protect, deleteComment)
 
 module.exports = router;
